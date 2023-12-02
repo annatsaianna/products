0e621421365da5f9957f46ecae3314f507a10494
annatsai <annatsaianna@gmail.com>
2023-12-02 14:14:05 +0800

    將次數轉成數字

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入價格: ')
+	price = int(price)#將字串轉成整數
 	products.append([name, price])
 print(products) 
 
@@ -11,7 +12,7 @@ for p in products:#p是指小清單
 	print(p[0], '的價格是', p[1])#小清單中商品名稱的價格
 
 
-with open ('products.csv', 'w')as f:
+with open ('products.csv', 'w')as f:#打開檔案，就算該檔案不存在，仍會重寫覆蓋出來
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1] )+ '\n')#字串用+號來合併，最後換行符號，真正的寫入,將價格整數轉換成字串再合併
 
